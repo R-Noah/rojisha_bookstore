def208996854f307df3866a41a644171760ee648
R-Noah <R.Kunwar4@wlv.ac.uk>
2025-11-27 07:29:50 +0545

    Add files via upload
    Add create book form and insert functionality

--- src/Models/Book.php
+++ src/Models/Book.php
@@ -22,4 +22,25 @@ class Book
         $stmt = $this->pdo->query($sql);
         return $stmt->fetchAll();
     }
+    public function create(array $data): bool
+{
+    $sql = "INSERT INTO books (title, author, genre, year_published, isbn, price, stock)
+            VALUES (:title, :author, :genre, :year_published, :isbn, :price, :stock)";
+
+    $stmt = $this->pdo->prepare($sql);
+
+    // Allow null price if empty
+    $price = ($data['price'] === '' ? null : $data['price']);
+
+    return $stmt->execute([
+        ':title'          => $data['title'],
+        ':author'         => $data['author'],
+        ':genre'          => $data['genre'],
+        ':year_published' => $data['year_published'],
+        ':isbn'           => $data['isbn'],
+        ':price'          => $price,
+        ':stock'          => $data['stock'],
+    ]);
+}
+
 }
